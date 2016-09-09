ac22792e4538473a350dfecab432716730e416bf
Aaron Leventhal <aleventhal@aisquared.com>
2016-09-09 08:25:26 -0400

    Remove

--- run-nomnom.sh
+++
@@ -1,6 +0,0 @@
-export AWS_ACCESS_KEY_ID='AKIAIE5T6LLRPEPL3WLA' 
-export AWS_SECRET_ACCESS_KEY='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' 
-cd /home/aleventhal/nomclean
-./nomnom-clean.sh
-cd /home/aleventhal/nom
-./nomnom.sh
